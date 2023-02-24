a5936518789b043703da2b77fe92b57aa0d95047
SinoLewis <sinolewis@gmail.com>
2023-02-24 15:15:13 +0300

    Admin Layout init.

--- src/components/ExploreContainer.vue
+++ src/components/ExploreContainer.vue
@@ -1,39 +1,29 @@
 <template>
-  <div id="container">
-    <strong>{{ name }}</strong>
-    <p>Explore <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
+  <div>
+    <sidebar />
+    <div class="relative md:ml-64 bg-blueGray-100">
+      <admin-navbar />
+      <header-stats />
+      <div class="px-4 md:px-10 mx-auto w-full -m-24">
+        <router-view />
+        <footer-admin />
+      </div>
+    </div>
   </div>
 </template>
 
-<script setup lang="ts">
-defineProps({
-  name: String,
-});
+<script lang="ts">
+import AdminNavbar from "@/components/layouts/AdminNavbar.vue";
+import Sidebar from "@/components/layouts/Sidebar.vue";
+import HeaderStats from "@/components/layouts/HeaderStats.vue";
+import FooterAdmin from "@/components/layouts/FooterAdmin.vue";
+export default {
+  name: "admin-layout",
+  components: {
+    AdminNavbar,
+    Sidebar,
+    HeaderStats,
+    FooterAdmin,
+  },
+};
 </script>
-
-<style scoped>
-#container {
-  text-align: center;
-  position: absolute;
-  left: 0;
-  right: 0;
-  top: 50%;
-  transform: translateY(-50%);
-}
-
-#container strong {
-  font-size: 20px;
-  line-height: 26px;
-}
-
-#container p {
-  font-size: 16px;
-  line-height: 22px;
-  color: #8c8c8c;
-  margin: 0;
-}
-
-#container a {
-  text-decoration: none;
-}
-</style>
